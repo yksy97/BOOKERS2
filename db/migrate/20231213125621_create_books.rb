0cdf80ee45a6787d5fd625feb961041b1f841475
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-13 13:35:27 +0000

    2023/12/13/22/35

--- db/migrate/20231213125621_create_books.rb
+++ db/migrate/20231213125621_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       
       # Bookモデルのテーブル設計：不安
-      t.string :user_name
+      # t.string :user_name
       t.string :title
       t.text :body
       t.integer :user_id
