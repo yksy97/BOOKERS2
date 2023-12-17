694a5d8193b230409002435841bd5db86457197e
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-17 06:07:15 +0000

    2023/12/17/15/00

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,8 @@ devise :database_authenticatable, :registerable,
         
         
         validates :name, uniqueness: true, length: { in: 2..20 }
-        validates :introduction, presence: true, length: { maximum: 50 }, on: :update
-        validates :profile_image, presence: true, on: :update
+        validates :introduction, length: { maximum: 50 }, on: :update
               
-
-#   def get_profile_image
-#     unless profile_image.attached?
-#       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-#       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-#     end
-#     profile_image.variant(resize_to_limit: [width, height]).processed
-#   end
-# end
-
 
 
 def get_profile_image(width, height)
@@ -32,4 +21,13 @@ def get_profile_image(width, height)
   profile_image.variant(resize_to_limit: [width, height]).processed
 end
 end
+
+
 # validatesの範囲を指定→　on: :action
+
+# ActiveStorage File Not Found Errorが起きた理由
+# ActiveStorageのエラーだったからActiveStorageに関連する記述ばかり見直していた
+# 結果的に、間違っていたのはvalidatesの設定だった。
+# validatesの設定で、ユーザー情報の編集においてintroductionとimageが空欄の場合にエラー文を表示する設定にしていた
+# しかし、そもそもintroductionとimageは空欄でOKだし、Sign Upの段階でintroductionとimageを設定する項目を設けていない
+# なので、Sign Up自体がバリデーションで弾かれてActiveStorage File Not Found Errorになっていた
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   get 'users/edit'
+
 devise_for :users
  
 resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
